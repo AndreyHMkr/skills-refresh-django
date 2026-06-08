8c5aa958069fe19fea9cc181e032ec2d0d49a262
AndreyHMkr <grushevoyandrey@gmail.com>
2026-06-08 22:37:20 +0100

    add views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
-# Create your views here.
+from tasks.models import Task
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "task_list.html"
+
+
+class TagListView(ListView):
+    model = Task
+    template_name = "tag_list.html"
+
+
+def toggle_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_done = not task.is_done
+    task.save()
+    return redirect("tasks:task_list")
+
+
+class AddTaskView(CreateView):
+    model = Task
+    fields = ["title", "deadline", "tags"]
+    template_name = "add_task.html"
+    success_url = reverse_lazy("tasks:task_list")
