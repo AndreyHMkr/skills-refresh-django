1da6b40612ce30b2fa2b7a616bbad1ba966becb2
AndreyHMkr <grushevoyandrey@gmail.com>
2026-06-08 22:35:09 +0100

    add Tag model

--- tasks/models.py
+++ tasks/models.py
@@ -7,3 +7,9 @@ class Task(models.Model):
     is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField("Tag", related_name="tags")
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
