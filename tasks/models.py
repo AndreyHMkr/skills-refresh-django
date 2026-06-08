88a77a35519bc30a3ad78bbb6742a5857c4077e6
AndreyHMkr <grushevoyandrey@gmail.com>
2026-06-08 22:34:43 +0100

    add Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", related_name="tags")
+
