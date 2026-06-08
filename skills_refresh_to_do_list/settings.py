e2ce014c815e86805032cd2d0f81ea2b87d474d0
AndreyHMkr <grushevoyandrey@gmail.com>
2026-06-08 22:36:17 +0100

    add STATICFILES_DIRS & tasks to installed app

--- skills_refresh_to_do_list/settings.py
+++ skills_refresh_to_do_list/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks'
 ]
 
 MIDDLEWARE = [
@@ -115,4 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
