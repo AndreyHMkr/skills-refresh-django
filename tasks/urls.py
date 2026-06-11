aba56965a083be14974bee987c510b8f5bcfdef7
AndreyHMkr <grushevoyandrey@gmail.com>
2026-06-11 21:59:07 +0100

    Add update and delete task routes

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path("tags/", TagListView.as_view(), name="tags"),
     path("toggle/<int:pk>/", views.toggle_task, name="toggle_task"),
     path("task/add/", AddTaskView.as_view(), name="add_task"),
+    path("task/update/<int:pk>/", views.update_task, name="update_task"),
+    path("task/delete/<int:pk>/", views.delete_task, name="delete_task"),
+
 ]
 
 app_name = 'tasks'
